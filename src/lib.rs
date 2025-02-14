9ab2956441b096e8f71f9ca8084bc0edcb421507
Scott T Weaver <scott.t.weaver@gmail.com>
2025-02-14 13:34:41 -0600

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,20 @@
-mod sources;
-pub mod component;
-pub(crate)  mod error;
-pub(crate) mod ext;
-pub(crate) mod prelude;
+mod prelude;
+mod error;
+mod parser;
+pub(crate) mod component;
+mod transformer;
 
-mod transform;
-mod segment;
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
