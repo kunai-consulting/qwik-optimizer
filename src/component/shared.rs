27481ce9dccb728f2029c68f6d0cc38d4fff238c
Scott T Weaver <scott.t.weaver@gmail.com>
2025-04-01 08:53:57 -0500

    Fix imports (#14)
    * Squash of previous WIP commits from "test-and-refactoring."
    
    * Fix known import issues.
    - Remove extraneous imports e.g. qrl import in components that don't use qrl.
    - Fix missing imports. The initial import_stack was too "shallow" so values, in some cases, were popped too soon.  The fix was simply initializing the import_stack with an initial row.
    - Dead import culling.
    
    * Formatting.
    
    * 'cargo fix'.
    
    * Formatting.

--- src/component/shared.rs
+++ src/component/shared.rs
@@ -1,7 +1,8 @@
 use crate::ext::AstBuilderExt;
 use oxc_allocator::{Allocator, FromIn, IntoIn};
-use oxc_ast::ast::Statement;
+use oxc_ast::ast::{ImportDeclarationSpecifier, ImportOrExportKind, Statement};
 use oxc_ast::AstBuilder;
+use oxc_span::SPAN;
 use std::convert::Into;
 use std::path::{Path, PathBuf};
 
@@ -17,18 +18,18 @@ pub const QRL_MARKER_IMPORTS: [&str; 2] = [QRL_MARKER, QRL_COMPONENT_MARKER];
 pub const PURE_ANNOTATION: &str = "/*#__PURE__*/";
 pub const PURE_ANNOTATION_LENGTH: u32 = PURE_ANNOTATION.len() as u32;
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum CommonImport {
-    BuilderIoQwik(Vec<String>),
+    BuilderIoQwik(Vec<ImportId>),
     Import(Import),
 }
 
 impl CommonImport {
     pub fn qrl() -> CommonImport {
-        CommonImport::BuilderIoQwik(vec![QRL.to_string()])
+        CommonImport::BuilderIoQwik(vec![QRL.into()])
     }
     pub fn component_qrl() -> CommonImport {
-        CommonImport::BuilderIoQwik(vec![COMPONENT_QRL.to_string()])
+        CommonImport::BuilderIoQwik(vec![COMPONENT_QRL.into()])
     }
 }
 
@@ -79,13 +80,89 @@ impl<'a> FromIn<'a, CommonExport> for Statement<'a> {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum ImportId {
+    Named(String),
+    NamedWithAlias(String, String),
+    Default(String),
+    Namespace(String),
+}
+
+impl From<&str> for ImportId {
+    fn from(value: &str) -> Self {
+        ImportId::Named(value.to_string())
+    }
+}
+
+impl From<&ImportDeclarationSpecifier<'_>> for ImportId {
+    fn from(value: &ImportDeclarationSpecifier<'_>) -> Self {
+        match value {
+            ImportDeclarationSpecifier::ImportSpecifier(specifier) => {
+                let imported = specifier.imported.name().to_string();
+                let local_name = specifier.local.name.to_string();
+                if imported == local_name {
+                    ImportId::Named(imported)
+                } else {
+                    ImportId::NamedWithAlias(imported, local_name)
+                }
+            }
+            ImportDeclarationSpecifier::ImportDefaultSpecifier(specifier) => {
+                let local_name = specifier.local.name.to_string();
+                ImportId::Default(local_name)
+            }
+            ImportDeclarationSpecifier::ImportNamespaceSpecifier(specifier) => {
+                let local_name = specifier.local.name.to_string();
+                ImportId::Namespace(local_name)
+            }
+        }
+    }
+}
+
+impl<'a> FromIn<'a, ImportId> for ImportDeclarationSpecifier<'a> {
+    fn from_in(value: ImportId, allocator: &'a Allocator) -> Self {
+        let ast = AstBuilder::new(allocator);
+        match value {
+            ImportId::Named(name) => {
+                let imported = ast.module_export_name_identifier_name(SPAN, &name);
+                let local_name = ast.binding_identifier(SPAN, &name);
+                ast.import_declaration_specifier_import_specifier(
+                    SPAN,
+                    imported,
+                    local_name,
+                    ImportOrExportKind::Value,
+                )
+            }
+
+            ImportId::NamedWithAlias(name, local_name) => {
+                let imported = ast.module_export_name_identifier_name(SPAN, &name);
+                let local_name = ast.binding_identifier(SPAN, &local_name);
+                ast.import_declaration_specifier_import_specifier(
+                    SPAN,
+                    imported,
+                    local_name,
+                    ImportOrExportKind::Value,
+                )
+            }
+            ImportId::Namespace(local_name) => {
+                let local_name = ast.binding_identifier(SPAN, &local_name);
+                ast.import_declaration_specifier_import_namespace_specifier(SPAN, local_name)
+            }
+            ImportId::Default(name) => {
+                let local_name = ast.binding_identifier(SPAN, &name);
+                ast.import_declaration_specifier_import_default_specifier(SPAN, local_name)
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Import {
-    names: Vec<String>,
+    names: Vec<ImportId>,
     source: PathBuf,
 }
+
 impl Import {
-    pub fn new<T: Into<PathBuf>>(names: Vec<String>, source: T) -> Self {
+    pub fn new<T: Into<PathBuf>>(names: Vec<ImportId>, source: T) -> Self {
         Self {
             names,
             source: source.into(),
@@ -123,7 +200,7 @@ impl Reference {
     }
     pub fn into_import<T: AsRef<Path>>(&self, source: T) -> Import {
         match self {
-            Reference::Variable(name) => Import::new(vec![name.clone()], source.as_ref()),
+            Reference::Variable(name) => Import::new(vec![name.as_str().into()], source.as_ref()),
         }
     }
 }
